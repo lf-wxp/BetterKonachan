a4852e5a87478e2f1d234beb1719cc9782d16666
Franciscowxp <franciscowxp@gmail.com>
2018-03-07 09:27:23 +0800

    fix netease music bug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('babel-core/register');
 require('babel-polyfill');
 const app = require('./app');
-const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8888;
 const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.listen(port, ip);
